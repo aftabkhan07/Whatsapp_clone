ba6217b6f21abbab1a9f866225ae46bfbe508a26
aftabkhan07 <khanaftaba1@gmail.com>
2023-08-25 23:05:08 +0530

    changes in onboardingPage

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,7 +1,15 @@
+import Avatar from "@/components/common/Avatar";
+import Input from "@/components/common/Input";
+import { useStateProvider } from "@/context/StateContext";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 function onboarding() {
+  const [{ userInfo }] = useStateProvider();
+  const [name, setName] = useState(userInfo?.name || "");
+  const [about, setAbout] = useState("");
+  const [image, setImage] = useState("/default_avatar.png");
+
   return (
     <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
       <div className="flex items-center justify-center gap-2">
@@ -13,7 +21,11 @@ function onboarding() {
       </div>
       <div className="flex gap-6 mt-6">
         <div className="flex flex-col items-center justif-center mt-5 gap-6">
-          
+          <Input name="Display Name" state={name} setState={setName} label/>
+          <Input name="About" state={about} setState={setAbout} label />
+        </div>
+        <div>
+          <Avatar type="xl" image={image} setImage={setImage} />
         </div>
       </div>
     </div>
