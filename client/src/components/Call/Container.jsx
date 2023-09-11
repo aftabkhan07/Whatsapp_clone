4d0e157f8c06a9282c4572b0f5dcb59564e5442b
aftabkhan07 <khanaftaba1@gmail.com>
2023-09-11 20:24:30 +0530

    added video/voice call sockets

--- client/src/components/Call/Container.jsx
+++ client/src/components/Call/Container.jsx
@@ -1,12 +1,41 @@
 import { useStateProvider } from "@/context/StateContext";
+import { reducerCases } from "@/context/constants";
+import Image from "next/image";
 import React, { useState } from "react";
+import { MdOutlineCallEnd } from "react-icons/md";
 
 function Container({ data }) {
-  const [{ userInfo, socket }] = useStateProvider();
+  const [{ userInfo, socket },dispatch] = useStateProvider();
   const [callAccepted, setCallAccepted] = useState(false);
+
+  const endCall = () => {
+    dispatch({ type: reducerCases.END_CALL });
+  };
+
   return (
     <div className="border-conversation-border border-l w-full bg-conversation-panel-background flex flex-col h-[100vh] overflow-hidden justify-center items-center text-white">
-      hey
+      <div className="flex flex-col gap-3 items-center">
+        <span className="text-5xl">{data.name}</span>
+        <span className="text-lg">
+          {callAccepted && data.callType !== "video"
+            ? "Ongoing Call"
+            : "Calling..."}
+        </span>
+      </div>
+      {(!callAccepted || data.callType === "voice") && (
+        <div className="my-24">
+          <Image
+            src={data.profilePicture}
+            alt="avatar"
+            height={300}
+            width={300}
+            className="rounded-full"
+          />
+        </div>
+      )}
+      <div className="h-16 w-16 bg-red-600 flex items-center justify-center rounded-full">
+        <MdOutlineCallEnd className="text-3xl cursor-pointer" onClick={endCall} />
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/uploads/recordings", express.static("uploads/recordings"))
-app.use("/uploads/images", express.static("uploads/images"))
+app.use("/uploads/recordings", express.static("uploads/recordings"));
+app.use("/uploads/images", express.static("uploads/images"));
 
 app.use("/api/auth", AuthRoutes);
 app.use("/api/messages", MessageRoutes);
@@ -30,9 +30,11 @@ const io = new Server(server, {
 global.onlineUsers = new Map();
 io.on("connection", (socket) => {
   global.chatSocket = socket;
+
   socket.on("add-user", (userId) => {
     onlineUsers.set(userId, socket.id);
   });
+
   socket.on("send-msg", (data) => {
     const sendUserSocket = onlineUsers.get(data.to);
     if (sendUserSocket) {
@@ -42,4 +44,46 @@ io.on("connection", (socket) => {
       });
     }
   });
+
+  socket.on("outgoing-voice-call", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("incoming-voice-call", {
+        from: data.from,
+        roomId: data.roomId,
+        callType: data.callType,
+      });
+    }
+  });
+
+  socket.on("outgoing-video-call", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("incoming-video-call", {
+        from: data.from,
+        roomId: data.roomId,
+        callType: data.callType,
+      });
+    }
+  });
+
+  socket.on("reject-voice-call", (data) => {
+    const sendUserSocket = onlineUsers.get(data.from);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("voice-call-rejected");
+    }
+  });
+
+  socket.on("reject-video-call", (data) => {
+    const sendUserSocket = onlineUsers.get(data.from);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("video-call-rejected");
+    }
+  });
+
+  socket.on("accept-incoming-call", (id) => {
+    const sendUserSocket = onlineUsers.get(id);
+    socket.to(sendUserSocket).emit("accept-call"); 
+  });
+
 });
